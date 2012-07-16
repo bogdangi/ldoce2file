7bc1a4b4106dd8075188aaf4b7ac2899f54fa543
bogdangi <bogdan.girman@gmail.com>
2012-07-16 07:36:13 +0300

    Added defenition and sense options

--- ldoce2file.py
+++ ldoce2file.py
@@ -21,6 +21,8 @@ class Ldoce(object):
         self.urllink = "http://www.ldoceonline.com"
         self.data = {}
         self.sense = sense
+        self.entry = None
+        self.entry_senses = None
         self.defenition = defenition
         self.params = {}
         self.soup = None
@@ -32,34 +34,42 @@ class Ldoce(object):
             self.params['q'] = self.word
 
     def find(self, selector):
-        result = self.soup.findAll('',selector)
+        result = self.entry.findAll('',selector)
+        return self._to_text(result)
+
+    def _to_text(self, result):
         if result != []:
             return [i.text for i in result]
-
         return ['']
 
-    def showDescription(self):
-        soup = self.choiseDescription()
-        self.soup = soup[0]
-        self.data = {
-                'head': {
-                    'word': self.find({'class':'HWD'})[0],
-                    'homographsSelected': self.find({'class':'homographsSelected'})[0],
-                    'POS': self.find({'class':'POS'})[0],
-                    },
-                'tag':self.tag,
-                'body': {
-                    'DEF': self.find({'class':'DEF'})[0],
-                    'EXAMPLE': self.find({'class':'EXAMPLE'}),
-                    }
+    def head(self):
+        return {'word': self.find({'class':'HWD'})[0],
+                'homographsSelected': self.find({'class':'homographsSelected'})[0],
+                'POS': self.find({'class':'POS'})[0],
                 }
+
+    def senses(self):
+        return [
+                { 'DEF': self._to_text(i.findAll('',{'class':'DEF'}))[0],
+                  'EXAMPLE': self._to_text(i.findAll('',{'class':'EXAMPLE'}))}
+                for i in self.entry_senses
+                ]
+
+
+    def showDescription(self):
+        self.choiseDescription()
+        print self.senses()
+        self.data = {
+                'head': self.head(),
+                'tag':  self.tag,
+                'senses': self.senses()                }
         self.rendered = render_to_string('./template.anki', self.data)
 
     def choiseDescription(self):
         req = urllib2.Request(self.urlsearch, urllib.urlencode(self.params))
         htmlSource = urllib2.urlopen(req).read()
         soup = BeautifulSoup(htmlSource)
-        choice = self.description
+        choice = self.defenition
         entry = soup.findAll('div', {'class':'Entry'})
         while entry == [] and choice != 0:
             words = soup.findAll('td',{'class':'hwdunSelMM'})
@@ -76,9 +86,9 @@ class Ldoce(object):
                 entry = soup.findAll('div', {'class':'Entry'})
 
         if self.is_short:
-            entry = self.choiseSense(BeautifulSoup(str(entry)))
+            self.choiseSense(BeautifulSoup(str(entry)))
         
-        return entry
+        self.entry = BeautifulSoup(str(entry))
 
     def choiseSense(self, soup):
         choice = self.sense
@@ -88,13 +98,24 @@ class Ldoce(object):
             if choice is None:
                 for i in rangeWords:
                     print i, words[i-1].text
-                choice = int(raw_input("Select sense: "))
-            if choice in rangeWords: 
-                soup.findAll('div', {'class':'Sense'})[choice-1]['id'] = 'markedSense' 
+                choice = int(raw_input("Select sense(s)(separeate ','): "))
+            try:
+                choice = choice.split(',')
+            except:
+                pass
+            print choice
+            choice = [i for i in rangeWords if i in rangeWords]
+            print choice
+            if choice: 
+                for i in choice:
+                    soup.findAll('div', {'class':'Sense'})[i-1]['id'] = 'markedSense' 
                 [i.extract() for i in words if i['id'] != 'markedSense']
                 break
+            else: choice = None
 
-        return soup.findAll('div', {'class':'Entry'})
+        self.entry_senses = [BeautifulSoup(str(i)) for i in soup.findAll('div', {'class':'Sense'})]
+        print self.entry_senses
+
 
 
 if __name__ == '__main__':
@@ -105,16 +126,22 @@ if __name__ == '__main__':
                               help="Words from file")
     parser.add_option("-s", "--short", dest="is_short", action="store_true", default=False,
                               help="Make short description")
-    parser.add_option("-R", "--sense", dest="sense", 
+    parser.add_option("-S", "--sense", dest="sense", default=None,
                               help="Select sense")
-    parser.add_option("-D", "--defenition", dest="defenition", 
+    parser.add_option("-D", "--defenition", dest="defenition", default=None,
                               help="Select defenition")
     parser.add_option("-t", "--tag", dest="tag", default='',
                               help="Set tag")
     (options, args) = parser.parse_args()
 
     if options.word is not None:
-        ldoce = Ldoce(options.word, is_short=options.is_short, tag=options.tag)
+        ldoce = Ldoce(
+                options.word, 
+                is_short   = options.is_short, 
+                tag        = options.tag, 
+                sense      = options.sense,
+                defenition = options.defenition,
+                )
         ldoce.showDescription()
         print ldoce.rendered
 
